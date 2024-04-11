95d677744f66ee8299124086d733c1ee1ba49b49
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2024-04-11 17:42:31 -0500

    conumiendo API

--- src/components/Pokemones.tsx
+++ src/components/Pokemones.tsx
@@ -4,8 +4,17 @@ export function Pokemones() {
   const [pokemones, setPokemones] = useState([])
 
   useEffect(() => {
-    const response = fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-    console.log(response)
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+        const data = await response.json()
+        console.log(data) // Aquí puedes ver los datos recibidos en la consola
+      } catch (error) {
+        console.error("Error fetching pokemones:", error)
+      }
+    }
+
+    fetchData()
   }, [])
 
   return <div>Pokemones</div>
